9cbd218a658beb18f8ec22d3f71e7ca7c32d49d3
Weverton Rodrigues <wevertonmixalt@gmail.com>
2020-02-24 03:43:58 -0300

    api e maps
    testando conexoes com api e estabelecendo modelos  de dados

--- src/components/CadastroPaciente/DadosLocais.tsx
+++ src/components/CadastroPaciente/DadosLocais.tsx
@@ -1,11 +1,43 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect, useMemo } from "react";
 import { View } from "react-native";
 import { useStyleSheet, Select, Layout } from "@ui-kitten/components";
 import PacienteContext from "../../contexts/PacienteContext";
-import { cidades, bairros } from "../../utils/constants";
+import api from "../../services/api";
 
 const DadosLocais = ({ navigation }) => {
   const { cidade, setCidade, bairro, setBairro } = useContext(PacienteContext);
+  const [cidades, setCidades] = useState([]);
+  const [bairros, setBairros] = useState([]);
+
+  useEffect(() => {
+    async function loadCidades() {
+      const response = await api.get("/acompanhamento/cidades");
+      const cidadesServ = response.data;
+      let result = cidadesServ.map(a => {
+        return {
+          text: a.nome
+        };
+      });
+      setCidades(result);
+    }
+    loadCidades();
+  }, []);
+
+  useEffect(() => {
+    async function loadBairros() {
+      const response = await api.get(
+        `/acompanhamento/bairros/{nomeCidade}?nomeCidade=${cidade}`
+      );
+      const bairrosServ = response.data;
+      let result = bairrosServ.map(a => {
+        return {
+          text: a.nome
+        };
+      });
+      setBairros(result);
+    }
+    loadBairros();
+  }, [setCidade]);
 
   const styles = useStyleSheet({
     lineContent: {
