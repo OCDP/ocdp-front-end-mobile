23a6424011509c7c98bc1a312d98b723cdfe6228
Matheus Braz <matheus.braz14@gmail.com>
2020-05-27 22:24:30 -0300

    Fix post resultados e mostrar img no historico de resultado
    tope

--- src/components/CadastroPaciente/Lesoes/Lesoes.tsx
+++ src/components/CadastroPaciente/Lesoes/Lesoes.tsx
@@ -3,7 +3,7 @@ import { View, Image } from "react-native";
 import { useStyleSheet, CheckBox, Text, Button } from "@ui-kitten/components";
 import { CardLesoes, TextHeader } from "./Lesoes.styles";
 
-const Lesoes = ({ navigation, title, imgRegiao }) => {
+const Lesoes = ({ navigation, title, nomeArquivo, imgRegiao, tipoAtendimento }) => {
   const styles = useStyleSheet({
     container: {
       paddingLeft: 36,
@@ -30,7 +30,17 @@ const Lesoes = ({ navigation, title, imgRegiao }) => {
           <TextHeader>{title}</TextHeader>
         </View>
         <View>
-          <Image style={styles.imageContent} source={{ uri: `${imgRegiao}` }} />
+          {tipoAtendimento == "RESULTADOS" ? (
+            <Image
+              style={{
+                width: 300,
+                height: 300,
+              }}
+              source={{
+                uri: `http://api-ocdp.us-east-2.elasticbeanstalk.com:8080/api/anexo/downloadFile/${nomeArquivo}`,
+              }}
+            />
+          ): <Image style={styles.imageContent} source={{ uri: `${imgRegiao}` }} />}
         </View>
       </CardLesoes>
     </View>
