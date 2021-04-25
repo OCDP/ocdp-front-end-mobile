1136d7235ef86f8d8c9146b661afe5c1033fe271
weverton <wevertonmixalt@gmail.com>
2021-04-25 16:15:27 -0300

    removendo decalracoes do spinner

--- sobrevidas/src/contexts/AppContext.tsx
+++ sobrevidas/src/contexts/AppContext.tsx
@@ -1,12 +1,10 @@
 import React, {
   createContext,
-  useState,
   Dispatch,
   SetStateAction,
   useContext,
 } from 'react';
 import useSecStorage from '../utils/useSecStorage';
-import Spinner from 'react-native-loading-spinner-overlay';
 
 const AppContext = createContext({} as Contexts.AppContext);
 
@@ -16,25 +14,15 @@ export const AppProvider: React.FC = ({children}) => {
     [],
   );
 
-  const [loading, setLoading] = useState(false);
-
   return (
     <AppContext.Provider
       value={{
         dadosPacientes,
         setDadosPacientes,
-        loading,
-        setLoading,
       }}>
-      <Spinner visible={loading} animation="fade" overlayColor="#fff9" />
       {children}
     </AppContext.Provider>
   );
-};
-
-export const useLoading = () => {
-  const {loading} = useContext(AppContext);
-  return loading;
 };
 
 export function useDadosPacientes(): [
