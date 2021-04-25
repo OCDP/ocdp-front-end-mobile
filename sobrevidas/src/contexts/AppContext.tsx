4fd5f806f3d2bf1f6599862e73072ccc96102bb8
Weverton Rodrigues <wevertonfr@discente.ufg.br>
2021-04-25 18:30:27 -0300

    limpando appcontext

--- sobrevidas/src/contexts/AppContext.tsx
+++ sobrevidas/src/contexts/AppContext.tsx
@@ -1,4 +1,3 @@
-import {Modal, Text} from '@ui-kitten/components';
 import React, {
   createContext,
   Dispatch,
@@ -6,17 +5,8 @@ import React, {
   useContext,
   useState,
 } from 'react';
-import {StyleSheet} from 'react-native';
-import Check from '../assets/img/Check';
-import Close from '../assets/img/Close';
+import ModalFeedback from '../components/ModalFeeback/ModalFeedback';
 import useSecStorage from '../utils/useSecStorage';
-import {
-  ButtonModalContainer,
-  LogoModalContainer,
-  ModalCard,
-  ModalCardContainer,
-  TextModalContainer,
-} from './AppContext.styles';
 
 const AppContext = createContext({} as Contexts.AppContext);
 
@@ -32,12 +22,6 @@ export const AppProvider: React.FC = ({children}) => {
     content: 'conteudo modal',
   });
 
-  const styles = StyleSheet.create({
-    backdrop: {
-      backgroundColor: 'rgba(0, 0, 0, 0.2)',
-    },
-  });
-
   return (
     <AppContext.Provider
       value={{
@@ -45,31 +29,10 @@ export const AppProvider: React.FC = ({children}) => {
         setDadosPacientes,
         setModal,
       }}>
-      <Modal
-        visible={modal.visible}
-        backdropStyle={styles.backdrop}
-        onBackdropPress={() => setModal({...modal, visible: false})}>
-        <ModalCardContainer level="1">
-          <ModalCard>
-            <LogoModalContainer>
-              {modal.type === 'error' ? (
-                <Close size={60} />
-              ) : (
-                <Check size={60} />
-              )}
-            </LogoModalContainer>
-
-            <TextModalContainer>
-              <Text category="s1">{modal.title}</Text>
-              <Text>{modal.content}</Text>
-            </TextModalContainer>
-          </ModalCard>
-          <ButtonModalContainer
-            onPress={() => setModal({...modal, visible: false})}>
-            Voltar
-          </ButtonModalContainer>
-        </ModalCardContainer>
-      </Modal>
+      <ModalFeedback
+        {...modal}
+        onClose={() => setModal({...modal, visible: false})}
+      />
       {children}
     </AppContext.Provider>
   );
