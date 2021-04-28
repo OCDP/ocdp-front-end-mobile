89ac7ce4ef08980f8784a25b6ea63e394b9c750a
weverton <wevertonmixalt@gmail.com>
2021-04-27 23:06:09 -0300

    nova page de dados do paciente

---
+++ sobrevidas/src/pages/DadosPacientePage/DadosPacientePage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageContainer from '../../components/PageContainer/PageContainer';
+import {HomeText} from './DadosPacientePage.styles';
+
+interface Props {}
+const DadosPacientePage: React.FC<Props> = ({navigation}: any) => {
+  return (
+    <PageContainer withFooter navigation={navigation}>
+      <HomeText>dados paciente page</HomeText>
+    </PageContainer>
+  );
+};
+
+export default DadosPacientePage;
