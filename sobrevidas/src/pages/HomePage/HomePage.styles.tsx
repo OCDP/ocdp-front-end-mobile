777b5a5d951009c6e221457fe47ad27d90fd4945
weverton <wevertonmixalt@gmail.com>
2021-04-29 11:42:57 -0300

    adicionando versao inicial do searchpacientes
    testando lodash e afins

--- sobrevidas/src/pages/HomePage/HomePage.styles.tsx
+++ sobrevidas/src/pages/HomePage/HomePage.styles.tsx
@@ -1,4 +1,9 @@
-import {Text} from '@ui-kitten/components';
+import {Input, Text} from '@ui-kitten/components';
 import styled from 'styled-components/native';
 
 export const HomeText = styled(Text)``;
+
+export const SearchPaciente = styled(Input)`
+  width: 100%;
+  padding: 8px 16px;
+`;
