1857c3598aca9ac3b6e2a98830c2fcb882fbcbe6
weverton <wevertonmixalt@gmail.com>
2021-05-03 01:22:09 -0300

    ajustando arquivos de icons

--- sobrevidas/src/components/icons.tsx
+++ sobrevidas/src/components/icons.tsx
@@ -5,67 +5,83 @@ import {Icon, IconElement} from '@ui-kitten/components';
 export const check: (style?: ImageStyle) => IconElement = style => (
   <Icon {...style} name="checkmark-outline" />
 );
+
 export const star: (style?: ImageStyle) => IconElement = style => (
   <Icon {...style} name="star" />
 );
+
 export const cancel: (style?: ImageStyle) => IconElement = style => (
   <Icon {...style} name="close-outline" />
 );
+
 export const back: (style?: ImageStyle) => IconElement = style => (
   <Icon {...style} name="arrow-back" />
 );
+
 export const home: (style?: ImageStyle) => IconElement = style => (
   <Icon {...style} name="home-outline" />
 );
+
 export const logout: (style?: ImageStyle) => IconElement = style => (
   <Icon {...style} name="log-out-outline" />
 );
+
 export const menuClose: (style?: ImageStyle) => IconElement = style => (
   <Icon {...style} name="arrowhead-up-outline" />
 );
+
 export const menu: (style?: ImageStyle) => IconElement = style => (
   <Icon {...style} name="menu" />
 );
+
 export const settings: (style?: ImageStyle) => IconElement = style => (
   <Icon {...style} name="settings-outline" />
 );
+
 export const card: (style?: ImageStyle) => IconElement = style => (
   <Icon {...style} name="credit-card-outline" />
 );
+
 export const shield: (style?: ImageStyle) => IconElement = style => (
   <Icon {...style} name="shield-outline" />
 );
+
 export const calendar: (style?: ImageStyle) => IconElement = style => (
   <Icon {...style} name="calendar-outline" />
 );
+
 export const phone: (style?: ImageStyle) => IconElement = style => (
   <Icon {...style} name="phone-call-outline" />
-);
-export const arrowMinimal = (style?: ImageStyle) => (
-  <Icon {...style} name="arrow-ios-forward-outline" />
 );
 
 export const search = (style?: ImageStyle) => (
   <Icon {...style} name="search-outline" />
 );
+
 export const unlock = (style?: ImageStyle) => (
   <Icon {...style} name="unlock-outline" />
 );
+
 export const enter = (style?: ImageStyle) => (
   <Icon {...style} name="log-in-outline" />
 );
+
 export const personOutline = (style?: ImageStyle) => (
   <Icon {...style} name="person-outline" />
 );
+
 export const copyPaste = (style?: ImageStyle) => (
   <Icon {...style} name="copy-outline" />
 );
+
 export const sendEmail = (style?: ImageStyle) => (
   <Icon {...style} name="email-outline" />
 );
+
 export const moreDetails = (style?: ImageStyle) => (
   <Icon {...style} name="menu-2-outline" />
 );
+
 export const person = (style?: ImageStyle) => (
   <Icon {...style} name="person-outline" />
 );
@@ -85,15 +101,19 @@ export const results = (style?: ImageStyle) => (
 export const heartOutline = (style?: ImageStyle) => (
   <Icon {...style} name="heart-outline" />
 );
+
 export const addButton = (style?: ImageStyle) => (
   <Icon {...style} name="plus-outline" />
 );
+
 export const pin = (style?: ImageStyle) => (
   <Icon {...style} name="pin-outline" />
 );
+
 export const map = (style?: ImageStyle) => (
   <Icon {...style} name="map-outline" />
 );
+
 export const activity = (style?: ImageStyle) => (
   <Icon {...style} name="activity-outline" />
 );
@@ -104,4 +124,8 @@ export const warning = (style?: ImageStyle) => (
 
 export const minimalBack = (style?: ImageStyle) => (
   <Icon {...style} name="arrow-ios-back-outline" />
+);
+
+export const minimalAvance = (style?: ImageStyle) => (
+  <Icon {...style} name="arrow-ios-forward-outline" />
 );
