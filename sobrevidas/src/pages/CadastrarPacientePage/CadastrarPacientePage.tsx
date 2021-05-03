ee4c9fc21a5db7a53db3c3496acca308eeba2e4f
weverton <wevertonmixalt@gmail.com>
2021-05-03 01:22:46 -0300

    nova pagina de cadastro de paciente com steps

--- sobrevidas/src/pages/CadastrarPacientePage/CadastrarPacientePage.tsx
+++ sobrevidas/src/pages/CadastrarPacientePage/CadastrarPacientePage.tsx
@@ -1,54 +1,46 @@
-import { Calendar, Input, NativeDateService, Select } from '@ui-kitten/components';
-import React, { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
+import React from 'react';
+import {useForm} from 'react-hook-form';
+import FieldSetDadosContato from '../../components/FieldSetDadosContato/FieldSetDadosContato';
+import FieldSetDadosEndereco from '../../components/FieldSetDadosEndereco/FieldSetDadosEndereco';
+import FieldSetDadosPessoais from '../../components/FieldSetDadosPessoais/FieldSetDadosPessoais';
 
-import { ScrollView, Text, View } from 'react-native';
 import PageContainer from '../../components/PageContainer/PageContainer';
-import ProgressSteps from '../../components/ProgressSteps/ProgressSteps';
-import { HomeText } from './CadastrarPacientePage.styles';
+import Steps from '../../components/Steps/Steps';
 
-interface Props { }
+interface Props {}
 
-const CadastrarPacientePage: React.FC<Props> = ({ navigation }: any) => {
-
-  const [date, setDate] = useState(new Date());
-  const { register, setValue, handleSubmit } = useForm()
-
-  useEffect(() => {
-    register('nome')
-    register('dataNascimento')
-    register('cpf')
-    register('sexo')
-    register('nomeMae')
-  }, [register])
+const CadastrarPacientePage: React.FC<Props> = ({navigation}: any) => {
+  const {register, setValue, handleSubmit, getValues} = useForm();
 
   return (
-    <PageContainer withHeader pageTitle="Cadastrar Paciente" navigation={navigation}>
-      <ProgressSteps size={3} step={3} flexInfo={0.05}></ProgressSteps>
-      <ScrollView style={{ flex: 1, paddingHorizontal: 12, paddingVertical: 12 }}>
-        <View style={{ paddingBottom: 12 }}>
-          <Input onChangeText={(newText => setValue('nome', newText))} label={"Nome"}></Input>
-        </View>
-        <View style={{ paddingBottom: 12 }}>
-          <Text>
-            Selected date: {date.toLocaleDateString()}
-          </Text>
-
-          <Calendar
-            date={date}
-            onSelect={nextDate => setDate(nextDate)}
-          />
-        </View>
-        <View style={{ paddingBottom: 12 }}>
-          <Input onChangeText={(newText => setValue('cpf', newText))} label={"Nome"}></Input>
-        </View>
-        <View style={{ paddingBottom: 12 }}>
-          <Select value={"aa"} onSelect={(newSelect => setValue('sexo', newSelect))} label={"Nome"}></Select>
-        </View>
-        <View style={{ paddingBottom: 12 }}>
-          <Input onChangeText={(newText => setValue('nomeMae', newText))} label={"Nome"}></Input>
-        </View>
-      </ScrollView>
+    <PageContainer
+      withHeader
+      canGoBack
+      pageTitle="Cadastrar Paciente"
+      navigation={navigation}>
+      <Steps
+        onComplete={handleSubmit(console.log)}
+        description={[
+          'Dados pessoais',
+          'Dados de contato',
+          'Dados de endereço',
+        ]}>
+        <FieldSetDadosPessoais
+          getValues={getValues}
+          register={register}
+          setValue={setValue}
+        />
+        <FieldSetDadosContato
+          getValues={getValues}
+          register={register}
+          setValue={setValue}
+        />
+        <FieldSetDadosEndereco
+          getValues={getValues}
+          register={register}
+          setValue={setValue}
+        />
+      </Steps>
     </PageContainer>
   );
 };
