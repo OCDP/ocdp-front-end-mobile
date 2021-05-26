c71d5d092af638d3658ee8878ac862cde48a734a
weverton <wevertonmixalt@gmail.com>
2021-05-26 01:32:25 -0300

    novas rotas de conexao

--- sobrevidas/src/hooks/networking/historico.ts
+++ sobrevidas/src/hooks/networking/historico.ts
@@ -5,12 +5,15 @@ import useHeaders from './useHeaders';
 export function useGetHistoricoPaciente() {
   const {auth} = useHeaders();
   return useCallback(
-    async <O extends Object>(cpfPaciente:string, params?: O, headers?: O) => {
-      return await api.get<Models.Historico[]>('/historico/atendimentos/cpf/' + cpfPaciente, {
-        params,
-        headers,
-        auth,
-      });
+    async <O extends Object>(id: string, params?: O, headers?: O) => {
+      return await api.get<Models.Historico[]>(
+        `/historico/atendimentos/paciente/id/${id}/`,
+        {
+          params,
+          headers,
+          auth,
+        },
+      );
     },
     [auth],
   );
