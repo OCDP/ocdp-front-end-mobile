2e35bd0d8426807d7818e4ddbb260a00005c126c
weverton <wevertonmixalt@gmail.com>
2021-05-26 02:43:49 -0300

    criando componente info list perfil

---
+++ sobrevidas/src/components/InfoListPerfil/InfoListPerfil.styles.tsx
@@ -0,0 +1,13 @@
+import {Layout} from '@ui-kitten/components';
+import styled from 'styled-components';
+
+export const InfoListPerfilContainer = styled(Layout)`
+  background-color: transparent;
+  margin: 16px 4px;
+`;
+
+export const InfoItemPerfil = styled(Layout)`
+  margin: 8px 16px;
+  background-color: transparent;
+  flex-direction: column;
+`;
