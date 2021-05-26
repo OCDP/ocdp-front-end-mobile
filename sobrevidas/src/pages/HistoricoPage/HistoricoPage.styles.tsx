275308b143ac7e916d0ca19dd528471663713d62
weverton <wevertonmixalt@gmail.com>
2021-05-26 01:33:53 -0300

    lixo lixo tchau

--- sobrevidas/src/pages/HistoricoPage/HistoricoPage.styles.tsx
+++ sobrevidas/src/pages/HistoricoPage/HistoricoPage.styles.tsx
@@ -1,23 +1,22 @@
-import { Button, Layout, Text } from '@ui-kitten/components';
-import { View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import {Button, Layout, Text} from '@ui-kitten/components';
+import {View} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
-import { buildStyledShadow } from '../../styles/buildShadow';
+import {buildStyledShadow} from '../../styles/buildShadow';
 
 const shadow = buildStyledShadow(6);
 
 export const HomeText = styled(Text)`
-    /* text-align: center; */
-    padding: 10px 20px
+  padding: 10px 20px;
 `;
 
 export const PaddingContent = styled(View)`
-    margin-bottom: 5%;
-    margin-top:5%;
-    align-self: center;
-    border-width: 1px;
-    border-color: gray;
-    background-color: white;
+  margin-bottom: 5%;
+  margin-top: 5%;
+  align-self: center;
+  border-width: 1px;
+  border-color: gray;
+  background-color: white;
 `;
 
 export const ItemListHistorico = styled(Layout)`
@@ -33,32 +32,39 @@ export const ItemListHistorico = styled(Layout)`
 `;
 
 export const HistoricoListContainer = styled(View)`
-    /* padding-top: 20px; */
-    flex-direction: row;
-    align-self: center;
-    padding-bottom: 1px;
-`
+  flex-direction: row;
+  align-self: center;
+  padding-bottom: 1px;
+`;
 
 export const BotaoContainer = styled(View)`
-  flex:1; 
-  flex-direction: row; 
-  align-self: center; 
-  padding-bottom: 10px;
-`
+  flex-direction: row;
+  justify-content: space-around;
+  padding: 8px 16px;
+`;
 
 export const HistoricoContainer = styled(ScrollView)`
   padding-top: 40px;
-`
+  flex: 1;
+`;
 
 export const BotaoNovaAcao = styled(Button)`
   margin-right: 1px;
   margin-left: 1px;
-`
+  border-radius: 20px;
+`;
 
-export const TimeLine = styled(View) <{ indice: number, indice_mais_recente: string, indices_anteriores: string }>`
-    ${props => `background-color: ${props.indice == 0 ? props.indice_mais_recente : props.indices_anteriores}; `}
-    width: 20px;
-`
+export const TimeLine = styled(View)<{
+  indice: number;
+  indice_mais_recente: string;
+  indices_anteriores: string;
+}>`
+  ${props =>
+    `background-color: ${
+      props.indice === 0 ? props.indice_mais_recente : props.indices_anteriores
+    }; `}
+  width: 20px;
+`;
 
 export const HistoricoInfos = styled(Layout)`
   background-color: transparent;
@@ -74,4 +80,3 @@ export const HistoricoNome = styled(Text)`
 export const HistoricoDetails = styled(Text)`
   font-size: 10px;
 `;
-
