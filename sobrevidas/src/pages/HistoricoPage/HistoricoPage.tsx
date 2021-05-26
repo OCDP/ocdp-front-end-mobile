3a783e11e97f744d40ef6846aa6969e1371778ca
weverton <wevertonmixalt@gmail.com>
2021-05-26 01:34:17 -0300

    mudando styles da home e historico

--- sobrevidas/src/pages/HistoricoPage/HistoricoPage.tsx
+++ sobrevidas/src/pages/HistoricoPage/HistoricoPage.tsx
@@ -1,68 +1,104 @@
-import { Button } from '@ui-kitten/components';
-import { Divider } from '@ui-kitten/components/ui/divider/divider.component';
-import { Text } from '@ui-kitten/components/ui/text/text.component';
-import React, { memo, useCallback, useContext, useEffect, useState } from 'react';
-import { ScrollView, View } from 'react-native';
-import { ItemListPaciente } from '../../components/ItemPaciente/ItemPaciente.styles';
+import React, {memo, useCallback, useContext, useState} from 'react';
+import {Alert} from 'react-native';
+import EmptyContent from '../../components/EmptyContent/EmptyContent';
+import FadeLoading from '../../components/FadeLoading/FadeLoading';
+import {addButton} from '../../components/icons';
 import PageContainer from '../../components/PageContainer/PageContainer';
-import { CadastroPacienteProvider, CadastroPacienteConsumer } from '../../contexts/CadastroPacienteContext';
 import UsuarioLogadoContext from '../../contexts/UsuarioLogadoContext';
-import { useGetHistoricoPaciente } from '../../hooks/networking/historico';
-import { HistoricoInfos, HistoricoDetails, ItemListHistorico, PaddingContent, HistoricoListContainer, TimeLine, HistoricoContainer, BotaoContainer, BotaoNovaAcao } from './HistoricoPage.styles';
+import {useGetHistoricoPaciente} from '../../hooks/networking/historico';
+import useMountEffect from '../../hooks/utils/useMountEffect';
+import getFirstName from '../../utils/getFirstName';
 
-interface Props { }
-const HistoricoPage: React.FC<Props> = ({ navigation, route }: any) => {
+import {
+  HistoricoInfos,
+  HistoricoDetails,
+  ItemListHistorico,
+  HistoricoListContainer,
+  TimeLine,
+  HistoricoContainer,
+  BotaoContainer,
+  BotaoNovaAcao,
+} from './HistoricoPage.styles';
 
+interface Props {}
+const HistoricoPage: React.FC<Props> = ({navigation, route}: any) => {
+  const {id, nome} = route.params;
   const getHistoricoPaciente = useGetHistoricoPaciente();
+  const [loading, setLoading] = useState(false);
 
-  
-  const [pacienteState, setPacienteState] = useState<Models.Paciente>(route.params.paciente)
   const [historico, setHistorico] = useState<Models.Historico[]>([]);
-  const { usuarioLogado, themeColors } = useContext(UsuarioLogadoContext);
+  const {usuarioLogado, themeColors} = useContext(UsuarioLogadoContext);
 
-  const historicos = useCallback(async () => {
+  const _getHistoricos = useCallback(async () => {
     try {
-      console.log(pacienteState.cpf)
-      const { data } = await getHistoricoPaciente(pacienteState.cpf);
-      console.log(data);
+      setLoading(true);
+      const {data} = await getHistoricoPaciente(id);
+      setLoading(false);
       setHistorico(data);
-    } catch (err) {
-      console.log(err);
+    } catch (e) {
+      setLoading(false);
+      Alert.alert('Erro na busca', 'Erro ao buscar histórico', [
+        {text: 'Voltar'},
+      ]);
     }
-  }, [getHistoricoPaciente])
+  }, [getHistoricoPaciente, id]);
 
-  useEffect(() => {
-    historicos();
-  }, [])
+  useMountEffect(_getHistoricos);
 
   return (
-    <PageContainer withHeader pageTitle={`Histórico ${pacienteState.nome}`} withFooter navigation={navigation} paciente={pacienteState}>
-      <HistoricoContainer>
-        <BotaoContainer style={{}}>
-          <BotaoNovaAcao onPress={() => navigation.navigate()}> Novo Atendimento </BotaoNovaAcao>
-          {usuarioLogado.nivelAtencao == "SECUNDARIA" && (
-            <BotaoNovaAcao> Nova Intervenção </BotaoNovaAcao>
-          )}
-        </BotaoContainer>
-        {historico.map((h, i) => (
-          <>
-
+    <PageContainer
+      withHeader
+      canGoBack
+      pageTitle={`Histórico de ${getFirstName(nome)}`}
+      navigation={navigation}>
+      <FadeLoading loading={loading} />
+      <BotaoContainer>
+        <BotaoNovaAcao
+          accessoryRight={addButton}
+          appearance="outline"
+          size="small"
+          onPress={() => navigation.navigate()}>
+          Novo Atendimento
+        </BotaoNovaAcao>
+        {usuarioLogado.nivelAtencao === 'SECUNDARIA' && (
+          <BotaoNovaAcao
+            accessoryRight={addButton}
+            appearance="outline"
+            size="small">
+            Nova Intervenção
+          </BotaoNovaAcao>
+        )}
+      </BotaoContainer>
+      {historico.length > 0 ? (
+        <HistoricoContainer>
+          {historico.map((item, i) => (
             <HistoricoListContainer>
-              <TimeLine indice={i} indice_mais_recente={themeColors["color-primary-300"]} indices_anteriores={themeColors["color-primary-200"]}></TimeLine>
+              <TimeLine
+                indice={i}
+                indice_mais_recente={themeColors['color-primary-300']}
+                indices_anteriores={themeColors['color-primary-200']}
+              />
               <ItemListHistorico>
                 <HistoricoInfos>
-                  <HistoricoDetails>Data de Atendimento: {h.dataAtendimento}</HistoricoDetails>
-                  <HistoricoDetails>{h.diferenca} atrás</HistoricoDetails>
-                  <HistoricoDetails>{h.localAtendimento}</HistoricoDetails>
-                  <HistoricoDetails>Profissional: {h.profissionalDeSaude}</HistoricoDetails>
-                  <HistoricoDetails>Atendimento {h.tipoAtendiemtento}</HistoricoDetails>
+                  <HistoricoDetails>
+                    Data de Atendimento: {item.dataAtendimento}
+                  </HistoricoDetails>
+                  <HistoricoDetails>{item.diferenca} atrás</HistoricoDetails>
+                  <HistoricoDetails>{item.localAtendimento}</HistoricoDetails>
+                  <HistoricoDetails>
+                    Profissional: {item.profissionalDeSaude}
+                  </HistoricoDetails>
+                  <HistoricoDetails>
+                    Atendimento {item.tipoAtendiemtento}
+                  </HistoricoDetails>
                 </HistoricoInfos>
               </ItemListHistorico>
             </HistoricoListContainer>
-          </>
-        ))}
-      </HistoricoContainer>
-
+          ))}
+        </HistoricoContainer>
+      ) : (
+        <EmptyContent emptyMessage="Nenhum item nesse histórico!" />
+      )}
     </PageContainer>
   );
 };
